44f21202f29cd5d3da7da8cd8c35eddddbdeba85
A. Alper Atıcı <alper.goplay@gmail.com>
2019-04-05 09:19:50 +0300

    fix NoSuchMethodError exception for Lolipop and KitKat

--- app/src/main/java/com/android/messaging/ui/PermissionCheckActivity.java
+++ app/src/main/java/com/android/messaging/ui/PermissionCheckActivity.java
@@ -54,7 +54,8 @@ public class PermissionCheckActivity extends Activity {
         }
 
         setContentView(R.layout.permission_check_activity);
-        UiUtils.setStatusBarColor(this, getColor(R.color.permission_check_activity_background));
+        if (OsUtil.isAtLeastM())
+            UiUtils.setStatusBarColor(this, getColor(R.color.permission_check_activity_background));
 
         findViewById(R.id.exit).setOnClickListener(new OnClickListener() {
             @Override
